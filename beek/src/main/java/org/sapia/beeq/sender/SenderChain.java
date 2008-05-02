7a8cd52a57f2f6370f4c177c541647ab64d0e3af
yanickduchesne@yahoo.com <yanickduchesne@yahoo.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2008-05-02 19:31:31 +0000

    refactored to support payload as binary type

--- beek/src/main/java/org/sapia/beeq/sender/SenderChain.java
+++ beek/src/main/java/org/sapia/beeq/sender/SenderChain.java
@@ -5,6 +5,15 @@ import java.util.List;
 
 import org.sapia.beeq.Message;
 
+/**
+ * This class implements a chain of responsibility. An instance
+ * of this class nests other {@link Sender} instances. All the ones
+ * that accept (see {@link Sender#accepts(Message)} a given message
+ * have their {@link Sender#send(Message)} method called.
+ * 
+ * @author yduchesne
+ *
+ */
 public class SenderChain implements Sender{
   
   private List<Sender> senders = new ArrayList<Sender>();
@@ -13,6 +22,11 @@ public class SenderChain implements Sender{
     senders.add(sender);
   }
   
+  /**
+   * This method returns <code>true</code> if at least one of
+   * the {@link Sender} that this instance encapsulates accepts
+   * the given message.
+   */
   public boolean accepts(Message msg) {
     for(Sender s:senders){
       if(s.accepts(msg)){
@@ -22,6 +36,10 @@ public class SenderChain implements Sender{
     return false;
   }
   
+  /**
+   * This method delegates sending of the message to all
+   * the {@link Sender}s that accept the given message.
+   */
   public void send(Message message) throws Exception {
     for(Sender s:senders){
       if(s.accepts(message)){
--- beek/src/main/java/org/sapia/beeq/sender/SenderException.java
+++ beek/src/main/java/org/sapia/beeq/sender/SenderException.java
@@ -1,5 +1,12 @@
 package org.sapia.beeq.sender;
 
+/**
+ * This exception is thrown when a message could not be send, for any
+ * reason.
+ *  
+ * @author yduchesne
+ *
+ */
 public class SenderException extends Exception {
   
   static final long serialVersionUID = 1L;
