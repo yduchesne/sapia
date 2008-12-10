5acedb822ca53d8f11c69f76a78b839440097b12
yanickduchesne@yahoo.com <yanickduchesne@yahoo.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2008-12-10 14:33:59 +0000

    added class for testing code generation

---
+++ tags/regis/fs0001/codegen/test/org/sapia/regis/sample/CodeGenTest.java
@@ -0,0 +1,36 @@
+package org.sapia.regis.sample;
+
+import java.io.File;
+import java.util.Properties;
+
+import org.sapia.regis.RegisSession;
+import org.sapia.regis.Registry;
+import org.sapia.regis.RegistryContext;
+
+import junit.framework.TestCase;
+
+public class CodeGenTest extends TestCase{
+  
+  public CodeGenTest(String name){
+    super(name);
+  }
+  
+  public void testLoad() throws Exception{
+    Properties props = new Properties();
+    
+    props.setProperty(RegistryContext.BOOTSTRAP, 
+        "etc/codegen/bootstrap.properties");
+    RegistryContext ctx = new RegistryContext(props);
+    Registry reg = ctx.connect();
+    RegisSession s = reg.open();
+        
+    SampleApp appConfig = SampleAppFactory.createFor(reg);
+    for(DatabasesNode node : appConfig.getDatabases().getDatabasesNodes()){
+      System.out.println(node.getUsername() + ", " + node.getUrl());
+    }
+    
+    s.close();
+    
+  }    
+
+}
