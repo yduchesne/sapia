30d95a7e0d39a537e7ee10e88cf26f2710896a00
yanickduchesne <yanickduchesne@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2009-05-08 13:03:17 +0000

    improved template caching

--- trunk/cocoon/maven/sample/src/test/java/org/sapia/cocoon/generation/chunk/template/TemplateCacheTest.java
+++ trunk/cocoon/maven/sample/src/test/java/org/sapia/cocoon/generation/chunk/template/TemplateCacheTest.java
@@ -18,10 +18,14 @@ public class TemplateCacheTest extends TestCase {
   protected void setUp() throws Exception {
     sources = new TestSourceResolver(new File("etc/template"));
     cache = new TestCache();
+    cache.setCleanupInterval(1000);
+    cache.setIdleTime(800);
+    cache.start();
   }
 
   protected void tearDown() throws Exception {
     super.tearDown();
+    cache.stop();
   }
   
   public void testRefresh() throws Exception{
@@ -29,11 +33,20 @@ public class TemplateCacheTest extends TestCase {
     String uri = "testTemplate.xml";
     cache.get(conf, uri, sources);
     Thread.sleep(100);
-    cache.getEntry(uri).touch();
+    cache.getEntry(uri, sources).touch();
     cache.get(conf, uri, sources);
     assertEquals(2, cache.refreshCount);
     cache.get(conf, uri, sources);
     assertEquals(2, cache.refreshCount);
+  }
+  
+  public void testCleanStaleEntries() throws Exception{
+    ParserConfig conf = new ParserConfig();
+    String uri = "testTemplate.xml";
+    cache.get(conf, uri, sources);
+    assertTrue(cache.containsEntry(uri, sources));
+    Thread.sleep(1500);
+    assertTrue(!cache.containsEntry(uri, sources));
   }
 
   static class TestCache extends TemplateCache{
