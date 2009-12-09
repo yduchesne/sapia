a0035cf76fc09dc5c7fb266bcf01d2b6421ba47c
yanickduchesne <yanickduchesne@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2009-12-09 15:46:39 +0000

    + support for comma-delimited list of files + files ending with an .xml extension are presumed exec configs

--- tags/corus/fs0003-script/server/java/src/org/sapia/corus/admin/cli/command/Deploy.java
+++ tags/corus/fs0003-script/server/java/src/org/sapia/corus/admin/cli/command/Deploy.java
@@ -10,65 +10,50 @@ import org.sapia.corus.deployer.ConcurrentDeploymentException;
 
 /**
  * @author Yanick Duchesne
- * <dl>
- * <dt><b>Copyright:</b><dd>Copyright &#169; 2002-2003 <a href="http://www.sapia-oss.org">Sapia Open Source Software</a>. All Rights Reserved.</dd></dt>
- * <dt><b>License:</b><dd>Read the license.txt file of the jar or visit the
- *        <a href="http://www.sapia-oss.org/license.html">license page</a> at the Sapia OSS web site</dd></dt>
- * </dl>
  */
 public class Deploy extends CorusCliCommand {
   
   public static final String OPT_EXEC_CONF = "e";
-
-  /**
-   * Constructor for Deploy.
-   */
-  public Deploy() {
-    super();
-  }
-  
 
   @Override
   protected void doExecute(CliContext ctx)
   throws AbortException, InputException {
     
     if(ctx.getCommandLine().isNextArg()){
-      deployDistribution(ctx);
+      String[] fileNames = ctx.getCommandLine().assertNextArg().getName().split(",");
+      for(String fileName:fileNames){
+        deployDistribution(ctx, fileName.trim());
+      }
     }
     else if(ctx.getCommandLine().containsOption(OPT_EXEC_CONF, true)){
-      deployExec(ctx);
+      deployExec(ctx, ctx.getCommandLine().assertOption(OPT_EXEC_CONF, true).getValue());
     }
     else{
       throw new InputException("File name expected as argument");
     }
   }
   
-  private void deployDistribution(CliContext ctx)
+  private void deployDistribution(CliContext ctx, String fileName)
   throws AbortException, InputException {
-    String fileName = null;
-    try {
-      fileName = ctx.getCommandLine().assertNextArg().getName();
-      displayProgress(ctx.getCorus().deploy(fileName,
-        getClusterInfo(ctx)),
-        ctx.getConsole());
-    } catch (ConcurrentDeploymentException e) {
-      ctx.getConsole().println("Distribution file already being deployed");
-    } catch (Exception e) {
-      ctx.getConsole().println("Problem deploying distribution");
-      e.printStackTrace(ctx.getConsole().out());
+    if(fileName.endsWith("xml")){
+      deployExec(ctx, fileName);
+    }
+    else{
+      try {
+        displayProgress(ctx.getCorus().deploy(fileName,
+          getClusterInfo(ctx)),
+          ctx.getConsole());
+      } catch (ConcurrentDeploymentException e) {
+        ctx.getConsole().println("Distribution file already being deployed");
+      } catch (Exception e) {
+        ctx.getConsole().println("Problem deploying distribution");
+        e.printStackTrace(ctx.getConsole().out());
+      }
     }
   }
   
-  private void deployExec(CliContext ctx) 
+  private void deployExec(CliContext ctx, String fileName) 
   throws AbortException, InputException {
-    String fileName = null;
-    try {
-      fileName = ctx.getCommandLine().assertOption(OPT_EXEC_CONF, true).getValue();
-    } catch (InputException e) {
-      
-      throw new InputException("File name expected as argument");
-    } 
-
     File file = new File(fileName);
     if(!file.exists()){
       ctx.getConsole().println("File not found: " + fileName);
