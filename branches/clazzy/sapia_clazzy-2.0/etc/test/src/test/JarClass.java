e572491cfa93fcce38c3683e755ce2954b3bfe5c
yanickduchesne <yanickduchesne@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2010-05-23 17:52:45 +0000

    [maven-release-plugin]  copy for tag sapia_clazzy-2.0

---
+++ branches/clazzy/sapia_clazzy-2.0/etc/test/src/test/JarClass.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.sapia.clazzy.test.Printable;
+
+
+public class JarClass implements Printable{
+  
+  public void print(){
+    System.out.println("Got jar class");
+  }
+  
+}
---
+++ branches/clazzy/sapia_clazzy-2.0/etc/test/src/test/JarClass2.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.sapia.clazzy.test.Printable;
+
+
+public class JarClass2 implements Printable{
+  
+  public void print(){
+    System.out.println("Got jar class #2");
+  }
+  
+}
