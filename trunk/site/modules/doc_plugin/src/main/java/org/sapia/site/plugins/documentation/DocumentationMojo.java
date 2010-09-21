d9030c3aeea3d4b5de7ab9788fb5d3db2644990a
yanickduchesne <yanickduchesne@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2010-09-21 01:20:50 +0000

    removed unused import

--- trunk/site/modules/doc_plugin/src/main/java/org/sapia/site/plugins/documentation/DocumentationMojo.java
+++ trunk/site/modules/doc_plugin/src/main/java/org/sapia/site/plugins/documentation/DocumentationMojo.java
@@ -1,12 +1,10 @@
 package org.sapia.site.plugins.documentation;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.UUID;
 
