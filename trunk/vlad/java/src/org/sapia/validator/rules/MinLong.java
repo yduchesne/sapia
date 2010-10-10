57762fc02828afbb3b047ad265a951c72778aced
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2010-10-10 20:40:05 +0000

    added project files

---
+++ trunk/vlad/java/src/org/sapia/validator/rules/MinLong.java
@@ -0,0 +1,33 @@
+package org.sapia.validator.rules;
+
+/**
+ * Asserts that the object to evaluate is greater or equal than a specified value.
+ * The evaluated object is expected to be an instance of <code>Long</code>
+ * 
+ * @author Yanick Duchesne
+ * <dl>
+ * <dt><b>Copyright:</b><dd>Copyright &#169; 2002-2003 <a href="http://www.sapia-oss.org">Sapia Open Source Software</a>. All Rights Reserved.</dd></dt>
+ * <dt><b>License:</b><dd>Read the license.txt file of the jar or visit the
+ *        <a href="http://www.sapia-oss.org/license.html">license page</a> at the Sapia OSS web site</dd></dt>
+ * </dl>
+ */
+public class MinLong extends AbstractMinMax{
+  private int    _val;
+  
+  /**
+   * Constructor for MinLong.
+   */
+  public MinLong() {
+    super(true);
+  }
+  
+  /**
+   * Sets the value against which the comparison should be peformed.
+   * 
+   * @param val a <code>long</code>.
+   */
+  public void setValue(long val){
+		super.setComparable(new Long(val));
+  }
+
+}
