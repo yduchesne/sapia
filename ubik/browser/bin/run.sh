528779ea40dcb2011bb624d0af0ab5a55a4c20a3
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2010-10-10 14:59:00 +0000

    [maven-release-plugin]  copy for tag sapia_regis-3.2

--- ubik/browser/bin/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-$JAVA_HOME/bin/java -cp ../conf/:./run.jar:../lib/log4j-1.2.8.jar:../lib/jms.jar:../../dist/sapia_ubik.jar:../../lib/jug.jar:../../lib/sapia_archie.jar:../../lib/sapia_taskman.jar org.ejtools.jndi.browser.Main
