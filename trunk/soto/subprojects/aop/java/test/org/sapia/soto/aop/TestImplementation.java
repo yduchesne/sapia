cb8d00aed84e1e3d2beb2164d33cbda6c49fc3f6
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2010-10-10 21:45:25 +0000

    added aop

---
+++ trunk/soto/subprojects/aop/java/test/org/sapia/soto/aop/TestImplementation.java
@@ -0,0 +1,23 @@
+package org.sapia.soto.aop;
+
+/**
+ * @author Yanick Duchesne
+ * 
+ * <dl>
+ * <dt><b>Copyright: </b>
+ * <dd>Copyright &#169; 2002-2003 <a href="http://www.sapia-oss.org">Sapia Open
+ * Source Software </a>. All Rights Reserved.</dd>
+ * </dt>
+ * <dt><b>License: </b>
+ * <dd>Read the license.txt file of the jar or visit the <a
+ * href="http://www.sapia-oss.org/license.html">license page </a> at the Sapia
+ * OSS web site</dd>
+ * </dt>
+ * </dl>
+ */
+public class TestImplementation implements java.io.Serializable, Comparable,
+    TestMarkerInterface {
+  public int compareTo(Object arg0) {
+    return 0;
+  }
+}
