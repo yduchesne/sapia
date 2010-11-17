4076df1900a39da61205f5cddd8e63339dfbda92
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2010-11-17 10:12:10 +0000

    fixed dependency bug

--- tags/corus/fs0004-maven/samples/corus_sample_grails_app/grails-app/conf/BuildConfig.groovy
+++ tags/corus/fs0004-maven/samples/corus_sample_grails_app/grails-app/conf/BuildConfig.groovy
@@ -12,11 +12,12 @@ grails.project.dependency.resolution = {
     repositories {        
         grailsPlugins()
         grailsHome()
+        grailsCentral() 
 
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
         //mavenLocal()
-        //mavenCentral()
+        mavenCentral()
         //mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
