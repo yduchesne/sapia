bf98e58b8b67281d4aca97c95b21ebb2e9f78963
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2010-12-22 18:34:26 +0000

    fixed corus status hook

--- tags/corus/fs0004-maven/samples/corus_sample_jetty/src/main/java/org/sapia/corus/sample/jetty/EmbeddedServer.java
+++ tags/corus/fs0004-maven/samples/corus_sample_jetty/src/main/java/org/sapia/corus/sample/jetty/EmbeddedServer.java
@@ -10,10 +10,11 @@ import org.sapia.corus.interop.Status;
 import org.sapia.corus.interop.api.InteropLink;
 import org.sapia.corus.interop.api.StatusRequestListener;
 
-public class EmbeddedServer {
+public class EmbeddedServer implements StatusRequestListener{
 
   private int port;
   private SessionManager sessionManager;
+  private StatisticsHandler stats;
   
   public EmbeddedServer(int port) {
     this(port, null);
@@ -27,8 +28,8 @@ public class EmbeddedServer {
   public void start() throws Exception{
     Server server = new Server(port);
     server.setStopAtShutdown(true);
-    final StatisticsHandler stats = new StatisticsHandler();
-        if(sessionManager != null){
+    stats = new StatisticsHandler();
+    if(sessionManager != null){
       SessionHandler sessionHandler = new SessionHandler(sessionManager);
       stats.setHandler(ServerUtil.loadWebapps(sessionHandler));
     }
@@ -39,36 +40,36 @@ public class EmbeddedServer {
     
     server.start();
     
-    InteropLink.getImpl().addStatusRequestListener(new StatusRequestListener() {
-      
-      @Override
-      public void onStatus(Status status) {
-        Context context = new Context("org.sapia.corus.sample.jetty");
-        context.addParam(createParam("dispatched", stats.getDispatched()));        
-        context.addParam(createParam("dispatchedActive", stats.getDispatchedActive()));
-        context.addParam(createParam("dispatchedActiveMax", stats.getDispatchedActiveMax()));        
-        context.addParam(createParam("dispatchedTimeMax", stats.getDispatchedTimeMax()));
-        context.addParam(createParam("dispatchedTimeTotal", stats.getDispatchedTimeTotal()));
-        context.addParam(createParam("dispatchedTimeMean", stats.getDispatchedTimeMean()));
-        context.addParam(createParam("requests", stats.getRequests()));        
-        context.addParam(createParam("requestsActive", stats.getRequestsActive()));        
-        context.addParam(createParam("requestsActiveMax", stats.getRequestsActiveMax()));        
-        context.addParam(createParam("requestsTimeMax", stats.getRequestTimeMax()));        
-        context.addParam(createParam("requestsTimeMean", stats.getRequestTimeMean()));
-        context.addParam(createParam("requestsTimeTotal", stats.getRequestTimeTotal()));
-        context.addParam(createParam("suspends", stats.getSuspends()));        
-        context.addParam(createParam("suspendsActive", stats.getSuspendsActive()));        
-        context.addParam(createParam("suspendsActiveMax", stats.getSuspendsActiveMax()));        
-      }
-      
-      
-      private Param createParam(String name, Object value){
-        return new Param(name, value.toString());
-      }
-    });
+    InteropLink.getImpl().addStatusRequestListener(this);
     
     System.out.println("*** Jetty server started successfully ***");
     server.join();
     System.out.println("*** Jetty server stopped ***");
   }
+
+  @Override
+  public void onStatus(Status status) {
+    Context context = new Context("org.sapia.corus.sample.jetty");
+    context.addParam(createParam("dispatched", stats.getDispatched()));        
+    context.addParam(createParam("dispatchedActive", stats.getDispatchedActive()));
+    context.addParam(createParam("dispatchedActiveMax", stats.getDispatchedActiveMax()));        
+    context.addParam(createParam("dispatchedTimeMax", stats.getDispatchedTimeMax()));
+    context.addParam(createParam("dispatchedTimeTotal", stats.getDispatchedTimeTotal()));
+    context.addParam(createParam("dispatchedTimeMean", stats.getDispatchedTimeMean()));
+    context.addParam(createParam("requests", stats.getRequests()));        
+    context.addParam(createParam("requestsActive", stats.getRequestsActive()));        
+    context.addParam(createParam("requestsActiveMax", stats.getRequestsActiveMax()));        
+    context.addParam(createParam("requestsTimeMax", stats.getRequestTimeMax()));        
+    context.addParam(createParam("requestsTimeMean", stats.getRequestTimeMean()));
+    context.addParam(createParam("requestsTimeTotal", stats.getRequestTimeTotal()));
+    context.addParam(createParam("suspends", stats.getSuspends()));        
+    context.addParam(createParam("suspendsActive", stats.getSuspendsActive()));        
+    context.addParam(createParam("suspendsActiveMax", stats.getSuspendsActiveMax()));
+    status.addContext(context);
+  }
+  
+  private Param createParam(String name, Object value){
+    return new Param(name, value.toString());
+  }
+  
 }
