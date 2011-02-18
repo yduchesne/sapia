88b20c4c559d6bbdc1cf269f2b388d172673253e
jcdesrochers <jcdesrochers@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2011-02-18 11:30:06 +0000

    Creating feature branch for scala and other language integration

---
+++ branches/fs0005-scala/magnet/src/main/java/org/sapia/magnet/MagnetDictionary.java
@@ -0,0 +1,29 @@
+package org.sapia.magnet;
+
+/**
+ * This interface act as a central dictionnary of namespace definitions of modules under
+ * the magnet umbrella.
+ *
+ * @author Jean-Cedric Desrochers
+ * <dl>
+ * <dt><b>Copyright:</b><dd>Copyright &#169; 2002-2003 <a href="http://www.sapia-oss.org">Sapia Open Source Software</a>. All Rights Reserved.</dd></dt>
+ * <dt><b>License:</b><dd>Read the license.txt file of the jar or visit the
+ *        <a href="http://www.sapia-oss.org/license.html" target="sapia-license">license page</a> at the Sapia OSS web site</dd></dt>
+ * </dl>
+ */
+public interface MagnetDictionary {
+
+  /** Defines the namespaces URI of Magnet. */
+  public static final String NAMESPACE_URI_MAGNET_OLD = "http://schemas.sapia-oss.org/magnet/core/";
+  public static final String NAMESPACE_URI_MAGNET = "http://schemas.sapia-oss.org/magnet/";
+
+  /** Defines the default namespaces prefix of Magnet. */
+  public static final String NAMESPACE_PREFIX_MAGNET = "MAGNET";
+
+  /** Defines the namespaces URI of Corus interoperability. */
+  public static final String NAMESPACE_URI_CORUS_INTEROP = "http://schemas.sapia-oss.org/corus/interoperability/";
+
+  /** Defines the default namespaces prefix of Corus interoperability. */
+  public static final String NAMESPACE_PREFIX_CORUS_INTEROP = "COR-IOP";
+
+}
