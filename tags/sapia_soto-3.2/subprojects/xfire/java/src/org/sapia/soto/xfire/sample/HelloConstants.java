20922ce770bccf5c908f9ff8c12c1bfa82e64b57
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2011-03-28 01:49:07 +0000

    Removed tags/sapia_soto-3.2

--- tags/sapia_soto-3.2/subprojects/xfire/java/src/org/sapia/soto/xfire/sample/HelloConstants.java
+++
@@ -1,8 +0,0 @@
-package org.sapia.soto.xfire.sample;
-
-public interface HelloConstants {
-  
-  public static final String NAMESPACE = "http://www.sapia-oss.org/soto/xfire/";
-  public static final String ENDPOINT_INTERFACE = "org.sapia.soto.xfire.sample.HelloService";  
-  
-}
