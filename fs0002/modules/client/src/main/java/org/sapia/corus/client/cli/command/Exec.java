6902c3b2c20c2c148e491c17e8f2d2c08283c4ab
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2011-08-09 16:47:00 +0000

    Copied trunk/corus to branches/corus/fs0002.

---
+++ fs0002/modules/client/src/main/java/org/sapia/corus/client/cli/command/Exec.java
@@ -0,0 +1,79 @@
+package org.sapia.corus.client.cli.command;
+
+import org.sapia.console.AbortException;
+import org.sapia.console.CmdLine;
+import org.sapia.console.InputException;
+import org.sapia.corus.client.ClusterInfo;
+import org.sapia.corus.client.cli.CliContext;
+import org.sapia.corus.client.cli.CliError;
+import org.sapia.corus.client.exceptions.processor.TooManyProcessInstanceException;
+
+
+/**
+ * @author Yanick Duchesne
+ */
+public class Exec extends CorusCliCommand {
+
+  public static final String OPT_EXEC_CONFIG = "e";
+  
+  @Override
+  protected void doExecute(CliContext ctx)
+                    throws AbortException, InputException {
+    
+    if(ctx.getCommandLine().containsOption(OPT_EXEC_CONFIG, true)){
+      doExecuteConfig(ctx);
+    }
+    else{
+      doExecuteProcesses(ctx);
+    }
+    
+  }
+
+  private void doExecuteConfig(CliContext ctx) throws AbortException, InputException {
+    ClusterInfo cluster = getClusterInfo(ctx);
+    String configName = ctx.getCommandLine().assertOption(OPT_EXEC_CONFIG, true).getValue();
+    displayProgress(ctx.getCorus().getProcessorFacade().exec(configName, cluster), ctx);
+  } 
+  private void doExecuteProcesses(CliContext ctx) throws AbortException, InputException {
+    String  dist      = null;
+    String  version   = null;
+    String  profile   = null;
+    String  vmName    = null;
+    int     instances = 1;
+    CmdLine cmd       = ctx.getCommandLine();
+
+    dist = cmd.assertOption(DIST_OPT, true).getValue();
+
+    version = cmd.assertOption(VERSION_OPT, true).getValue();
+
+    profile = cmd.assertOption(PROFILE_OPT, true).getValue();
+
+    if (cmd.containsOption(VM_NAME_OPT, true)) {
+      vmName = cmd.assertOption(VM_NAME_OPT, true).getValue();
+    }
+
+    if (cmd.containsOption(VM_INSTANCES, true)) {
+      instances = cmd.assertOption(VM_INSTANCES, true).asInt();
+    }
+
+    ClusterInfo cluster = getClusterInfo(ctx);
+
+    try{
+      if (vmName != null) {
+        displayProgress(
+                ctx.getCorus().getProcessorFacade().exec(
+                        dist, version, profile, vmName, instances, cluster),
+                ctx);
+      } else {
+        displayProgress(
+                ctx.getCorus().getProcessorFacade().exec(
+                        dist, version, profile, instances, cluster),
+                ctx);
+      }
+    }catch(TooManyProcessInstanceException e){
+      CliError err = ctx.createAndAddErrorFor(this, e);
+      ctx.getConsole().println(err.getSimpleMessage());
+    }
+  }
+  
+}
