01a16fbd25ac79cc45b505d6f77048fc8f594bd9
yanickduchesne <yanickduchesne@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2012-04-10 03:06:27 +0000

    added examples

---
+++ trunk/ubik/examples/src/main/java/org/sapia/ubik/rmi/examples/nio/NioFoo.java
@@ -0,0 +1,33 @@
+package org.sapia.ubik.rmi.examples.nio;
+
+import java.util.Properties;
+
+import org.sapia.ubik.rmi.Consts;
+import org.sapia.ubik.rmi.examples.UbikFoo;
+import org.sapia.ubik.rmi.server.Hub;
+import org.sapia.ubik.rmi.server.transport.nio.tcp.NioAddress;
+import org.sapia.ubik.rmi.server.transport.nio.tcp.NioTcpTransportProvider;
+
+public class NioFoo {
+
+  public static void main(String[] args) {
+    try {
+      //Log.setDebug();
+      Properties props = new Properties();
+      props.setProperty(Consts.TRANSPORT_TYPE,
+        NioTcpTransportProvider.TRANSPORT_TYPE);
+      props.setProperty(NioTcpTransportProvider.PORT, "6060");
+      props.setProperty(Consts.SERVER_MAX_THREADS, "10");      
+      
+      Hub.exportObject(new UbikFoo(), props);
+      
+      System.out.println("Server started");
+
+      while (true) {
+        Thread.sleep(100000);
+      }
+    } catch (Throwable t) {
+      t.printStackTrace();
+    }
+  }  
+}
