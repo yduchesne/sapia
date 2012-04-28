5ecc2fff40bedacb01ac915df57f4d6e5c4e18df
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2012-04-28 01:56:00 +0000

    added property(name, value) method

--- trunk/ubik/core/src/main/java/org/sapia/ubik/rmi/server/transport/nio/tcp/NioServerExporter.java
+++ trunk/ubik/core/src/main/java/org/sapia/ubik/rmi/server/transport/nio/tcp/NioServerExporter.java
@@ -15,116 +15,128 @@ import org.sapia.ubik.util.Strings;
  *
  */
 public class NioServerExporter {
-	
-	private int 	port;
-	private String 	bindAddress;
-	private int 	bufferSize 		= Consts.DEFAULT_MARSHALLING_BUFSIZE;
-	private int     maxThreads;
-	
-	/**
-	 * @return the buffer size to use when performing marshalling/unmarshalling. Defaults to {@link Consts#DEFAULT_MARSHALLING_BUFSIZE}.
-	 */
-	public int getBufferSize() {
-	  return bufferSize;
+  
+  private int        port;
+  private String     bindAddress;
+  private int        bufferSize     = Consts.DEFAULT_MARSHALLING_BUFSIZE;
+  private int        maxThreads;
+  private Properties props          = new Properties();
+  
+  /**
+   * @return the buffer size to use when performing marshalling/unmarshalling. Defaults to {@link Consts#DEFAULT_MARSHALLING_BUFSIZE}.
+   */
+  public int getBufferSize() {
+    return bufferSize;
   }
-	
-	/** 
-	 * @param bufferSize a buffer size.
-	 * @return this instance.
-	 */
-	public NioServerExporter setBufferSize(int bufferSize) {
-	  this.bufferSize = bufferSize;
-	  return this;
+  
+  /** 
+   * @param bufferSize a buffer size.
+   * @return this instance.
+   */
+  public NioServerExporter setBufferSize(int bufferSize) {
+    this.bufferSize = bufferSize;
+    return this;
   }
-	
-	/**
-	 * @return the port to which remote objects should be bound (a random port will be used if not specified).
-	 */
-	public int getPort() {
-	  return port;
+  
+  /**
+   * @return the port to which remote objects should be bound (a random port will be used if not specified).
+   */
+  public int getPort() {
+    return port;
   }
-	
-	/**
-	 * @param port a port to assign to this instance.
-	 * @return this instance.
-	 * @see #getPort()
-	 */
-	public NioServerExporter port(int port) {
-	  this.port = port;
-	  return this;
+  
+  /**
+   * @param port a port to assign to this instance.
+   * @return this instance.
+   * @see #getPort()
+   */
+  public NioServerExporter port(int port) {
+    this.port = port;
+    return this;
   }
-	
-	/**
-	 * @return this instance's bind address (remote objects will be exported to all network interfaces if not specified).
-	 */
-	public String getBindAddress() {
-	  return bindAddress;
+  
+  /**
+   * @return this instance's bind address (remote objects will be exported to all network interfaces if not specified).
+   */
+  public String getBindAddress() {
+    return bindAddress;
   }
-	
-	/**
-	 * @param bindAddress the network address to which remote objects should be bound.
-	 * @return this instance.
-	 * @see #getBindAddress()
-	 */
-	public NioServerExporter bindAddress(String bindAddress) {
-	  this.bindAddress = bindAddress;
-	  return this;
+  
+  /**
+   * @param bindAddress the network address to which remote objects should be bound.
+   * @return this instance.
+   * @see #getBindAddress()
+   */
+  public NioServerExporter bindAddress(String bindAddress) {
+    this.bindAddress = bindAddress;
+    return this;
   }
-	
-	/**
-	 * @return the maximum number of worker threads that should be used by servers through which the remote 
-	 * objects this instance creates are exported (no maximum is defined by default).
-	 */
-	public int getMaxThreads() {
-		return maxThreads;
-	}
-	
-	/**
-	 * @param maxThreads a maximum number of worker threads.
-	 * @return this instance
-	 * @see #getMaxThreads()
-	 */
-	public NioServerExporter maxThreads(int maxThreads) {
-	  this.maxThreads = maxThreads;
-	  return this;
+  
+  /**
+   * @return the maximum number of worker threads that should be used by servers through which the remote 
+   * objects this instance creates are exported (no maximum is defined by default).
+   */
+  public int getMaxThreads() {
+    return maxThreads;
   }
-	
-	/**
-	 * This method internally calls {@link Hub#exportObject(Object, Properties)} to export the
-	 * given object, setting this instance's configuration as properties that are passed to the method.
-	 * <p>
-	 * It returns the stub resulting from the export operation.
-	 * 
-	 * @param toExport an {@link Object} to export.
-	 * @return the stub that was created.
-	 * 
-	 * @throws RemoteException if a problem occurred trying to export the object.
-	 */
-	public Object export(Object toExport) throws RemoteException {
-		Properties props = new Properties();
-		props.setProperty(Consts.TRANSPORT_TYPE, NioTcpTransportProvider.TRANSPORT_TYPE);
-		if(port > 0) {
-			props.setProperty(NioTcpTransportProvider.PORT, Integer.toString(port));
-		}
-		if(bindAddress != null) {
-			props.setProperty(NioTcpTransportProvider.BIND_ADDRESS, bindAddress);
-		}
-		if(bufferSize > 0) {
-			props.setProperty(Consts.MARSHALLING_BUFSIZE, Integer.toString(bufferSize));
-		}
-		if(maxThreads > 0) {
-			props.setProperty(Consts.SERVER_MAX_THREADS, Integer.toString(maxThreads));
-		}
-		
-		return Hub.exportObject(toExport, props);
-	}
+  
+  /**
+   * @param maxThreads a maximum number of worker threads.
+   * @return this instance
+   * @see #getMaxThreads()
+   */
+  public NioServerExporter maxThreads(int maxThreads) {
+    this.maxThreads = maxThreads;
+    return this;
+  }
+  
+  /**
+   * Internally adds a property to this instance's encapsulated {@link Properties}.
+   * 
+   * @param name the name of the property to add.
+   * @param value the property value.
+   * @return this instance.
+   */
+  public NioServerExporter property(String name, String value) {
+    props.setProperty(name, value);
+    return this;
+  }
+  
+  /**
+   * This method internally calls {@link Hub#exportObject(Object, Properties)} to export the
+   * given object, setting this instance's configuration as properties that are passed to the method.
+   * <p>
+   * It returns the stub resulting from the export operation.
+   * 
+   * @param toExport an {@link Object} to export.
+   * @return the stub that was created.
+   * 
+   * @throws RemoteException if a problem occurred trying to export the object.
+   */
+  public Object export(Object toExport) throws RemoteException {
+    props.setProperty(Consts.TRANSPORT_TYPE, NioTcpTransportProvider.TRANSPORT_TYPE);
+    if(port > 0) {
+      props.setProperty(NioTcpTransportProvider.PORT, Integer.toString(port));
+    }
+    if(bindAddress != null) {
+      props.setProperty(NioTcpTransportProvider.BIND_ADDRESS, bindAddress);
+    }
+    if(bufferSize > 0) {
+      props.setProperty(Consts.MARSHALLING_BUFSIZE, Integer.toString(bufferSize));
+    }
+    if(maxThreads > 0) {
+      props.setProperty(Consts.SERVER_MAX_THREADS, Integer.toString(maxThreads));
+    }
+    
+    return Hub.exportObject(toExport, props);
+  }
 
-	public String toString() {
-		return Strings.toString(
-				"port", port, 
-				"bindAddress", bindAddress, 
-				"bufferSize", bufferSize, 
-				"maxThreads", maxThreads
-		);
-	}
+  public String toString() {
+    return Strings.toString(
+        "port", port, 
+        "bindAddress", bindAddress, 
+        "bufferSize", bufferSize, 
+        "maxThreads", maxThreads
+    );
+  }
 }
