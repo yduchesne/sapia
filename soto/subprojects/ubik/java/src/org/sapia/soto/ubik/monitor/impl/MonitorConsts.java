3ee97cbde570f585fbbed1147095c77f3afc47b0
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2012-05-06 01:33:21 +0000

    Copied trunk/vlad to branches/vlad/terencek01.

--- soto/subprojects/ubik/java/src/org/sapia/soto/ubik/monitor/impl/MonitorConsts.java
+++
@@ -1,7 +0,0 @@
-package org.sapia.soto.ubik.monitor.impl;
-
-public interface MonitorConsts {
-
-  public static final String DISCO_ADMIN = "soto.ubik.monitor.admin";
-  public static final String DISCO_AGENT = "soto.ubik.monitor.agent";
-}
