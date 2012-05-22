3c92d28957d3c4a0092f28d3066c258943e4f094
jc.desrochers <jc.desrochers@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2012-05-22 01:26:05 +0000

    Initial version of new bat files (to be tested)

--- trunk/ubik/core/bin/jndi.bat
+++ trunk/ubik/core/bin/jndi.bat
@@ -1,6 +1,31 @@
-@echo off
+echo off
+cls
 
-set classpath=../dist/sapia_ubik.jar;../lib/sapia_taskman.jar;../lib/jug.jar
+if not "%JAVA_HOME%"=="" goto okJavaHome
 
-java org.sapia.ubik.rmi.naming.remote.JNDIServer %1 %2 %3 %4
+JAVA_HOME environment variable not set.
+Set this variable to point to your Java
+installation directory.
 
+goto end
+
+:okJavaHome
+
+SET LOCALCLASSPATH=
+
+if "%LOCALCLASSPATH_DEFINED%"=="true" goto okLcp
+
+for %%i in (..\lib\*.jar) do call lcp.bat %%i
+for %%i in (..\dist\*.jar) do call lcp.bat %%i
+
+set LOCALCLASSPATH_DEFINED=true
+
+:okLcp
+
+rem echo %LOCALCLASSPATH%
+
+set APPCLASSPATH=%CLASSPATH%;%LOCALCLASSPATH%;%UBIK_CP%
+
+"%JAVA_HOME%\bin\java" %UBIK_OPTS% -classpath "%APPCLASSPATH%" org.sapia.ubik.rmi.naming.remote.JNDIServer %*
+
+:end
