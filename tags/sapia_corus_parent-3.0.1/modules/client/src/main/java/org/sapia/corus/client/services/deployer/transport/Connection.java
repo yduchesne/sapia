6b66a9f5b00981b489ce9b6b71caadc71b06474f
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2012-10-13 14:53:34 +0000

    [maven-release-plugin]  copy for tag sapia_corus_parent-3.0.1

---
+++ tags/sapia_corus_parent-3.0.1/modules/client/src/main/java/org/sapia/corus/client/services/deployer/transport/Connection.java
@@ -0,0 +1,33 @@
+package org.sapia.corus.client.services.deployer.transport;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Models a network, stream-based connection.
+ * 
+ * @author Yanick Duchesne
+ */
+public interface Connection {
+	
+	/**
+	 * @return this client's {@link InputStream}.
+	 * @throws IOException
+	 */
+	public InputStream getInputStream() throws IOException;
+	
+	
+	/**
+	 * @return this client's {@link OutputStream}.
+	 * @throws IOException
+	 */
+	public OutputStream getOutputStream() throws IOException;
+	
+	/**
+	 * Closes this connection.
+	 */
+	public void close();
+	
+
+}
