00e482a136ecac3fb4ff0d7526d43c2caded657f
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2012-10-24 02:36:22 +0000

    [maven-release-plugin]  copy for tag sapia_ubik_parent-3.1

--- cocoon/maven/sample/src/main/java/org/sapia/cocoon/generation/chunk/template/content/TemplateContent.java
+++
@@ -1,8 +0,0 @@
-package org.sapia.cocoon.generation.chunk.template.content;
-
-import org.sapia.cocoon.generation.chunk.template.TemplateContext;
-
-public interface TemplateContent {
-
-  public String render(TemplateContext ctx);
-}
