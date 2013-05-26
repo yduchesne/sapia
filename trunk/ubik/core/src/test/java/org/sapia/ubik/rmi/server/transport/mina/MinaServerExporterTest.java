0d0877ea57d1dbfba186c908b474307086207105
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2013-05-26 15:30:44 +0000

    re-added problematic classes (svn state inconsistent)

---
+++ trunk/ubik/core/src/test/java/org/sapia/ubik/rmi/server/transport/mina/MinaServerExporterTest.java
@@ -0,0 +1,50 @@
+package org.sapia.ubik.rmi.server.transport.mina;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.sapia.ubik.concurrent.Counter;
+import org.sapia.ubik.rmi.Consts;
+import org.sapia.ubik.rmi.server.Hub;
+import org.sapia.ubik.util.PropertiesUtil;
+
+public class MinaServerExporterTest {
+	
+	@Before
+	public void setUp() {
+    PropertiesUtil.clearUbikSystemProperties();	  
+		Hub.shutdown();
+		System.setProperty(Consts.COLOCATED_CALLS_ENABLED, "false");
+	}
+	
+	@After
+	public void tearDown() {
+    PropertiesUtil.clearUbikSystemProperties();	  
+		Hub.shutdown();
+	}
+
+  @Test
+	public void testExport() throws Exception {
+		
+		final Counter counter = new Counter(2);
+		MinaServerExporter exporter = new MinaServerExporter();
+		TestInterface remoteObject = (TestInterface) exporter.export(new TestInterface() {
+			@Override
+			public void test() {
+				counter.increment();
+			}
+		});
+		
+		remoteObject.test();
+		remoteObject.test();
+		assertEquals(2, counter.getCount());
+	}
+	
+	public interface TestInterface {
+		
+		public void test();
+	}
+
+}
---
+++ trunk/ubik/core/src/test/java/org/sapia/ubik/rmi/server/transport/mina/NioServerExporterTest.java
@@ -0,0 +1,52 @@
+package org.sapia.ubik.rmi.server.transport.mina;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.sapia.ubik.concurrent.Counter;
+import org.sapia.ubik.rmi.Consts;
+import org.sapia.ubik.rmi.server.Hub;
+import org.sapia.ubik.rmi.server.transport.netty.NettyServerExporter;
+import org.sapia.ubik.util.PropertiesUtil;
+
+public class NioServerExporterTest {
+	
+	@Before
+	public void setUp() {
+    PropertiesUtil.clearUbikSystemProperties();	  
+		Hub.shutdown();
+		System.setProperty(Consts.COLOCATED_CALLS_ENABLED, "false");
+	}
+	
+	@After
+	public void tearDown() {
+    PropertiesUtil.clearUbikSystemProperties();	  
+		Hub.shutdown();
+		System.clearProperty(Consts.COLOCATED_CALLS_ENABLED);
+	}
+
+  @Test
+	public void testExport() throws Exception {
+		
+		final Counter counter = new Counter(2);
+		NettyServerExporter exporter = new NettyServerExporter();
+		TestInterface remoteObject = (TestInterface) exporter.export(new TestInterface() {
+			@Override
+			public void test() {
+				counter.increment();
+			}
+		});
+		
+		remoteObject.test();
+		remoteObject.test();
+		assertEquals(2, counter.getCount());
+	}
+	
+	public interface TestInterface {
+		
+		public void test();
+	}
+
+}
