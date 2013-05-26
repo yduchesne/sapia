e7359830433f526a4e169a6f50e9ef64af317d02
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2013-05-26 17:04:38 +0000

    [maven-release-plugin]  copy for tag sapia_corus_parent-4.0

---
+++ tags/sapia_corus_parent-4.0/modules/client/src/main/java/org/sapia/corus/client/exceptions/cli/SystemExitException.java
@@ -0,0 +1,15 @@
+package org.sapia.corus.client.exceptions.cli;
+
+import org.sapia.console.AbortException;
+
+/**
+ * Thrown from within the CLI implementation to trigger exit.
+ * 
+ * @author yduchesne
+ *
+ */
+public class SystemExitException extends AbortException {
+	
+	static final long serialVersionUID = 1L;
+
+}
