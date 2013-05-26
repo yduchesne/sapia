84575fb866a66fb3e91e2caf01d14ee979497458
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2013-05-26 15:30:44 +0000

    re-added problematic classes (svn state inconsistent)

---
+++ ubik/core/src/test/java/org/sapia/ubik/rmi/server/transport/mina/NioResponseEncoderTest.java
@@ -0,0 +1,77 @@
+package org.sapia.ubik.rmi.server.transport.mina;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.mina.common.IoSession;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.sapia.ubik.rmi.server.transport.MarshalStreamFactory;
+import org.sapia.ubik.rmi.server.transport.mina.NioResponse;
+import org.sapia.ubik.rmi.server.transport.mina.NioResponseEncoder;
+
+public class NioResponseEncoderTest {
+	
+	private NioResponseEncoder  encoder;
+	private IoSession 					session;
+	private Map<String, Object> sessionData;
+	private TestEncoderOutput   output;
+
+	@Before
+  public void setUp() throws Exception {
+		encoder     = new NioResponseEncoder();
+		sessionData = new HashMap<String, Object>();
+		session 		= mock(IoSession.class);
+		
+		when(session.getAttribute(anyString())).thenAnswer(new Answer<Object>() {
+			@Override
+			public Object answer(InvocationOnMock invocation) throws Throwable {
+			  return sessionData.get(invocation.getArguments()[0]);
+			}
+		});
+		
+		when(session.setAttribute(anyString(), anyObject())).thenAnswer(new Answer<Object>() {
+			@Override
+			public Object answer(InvocationOnMock invocation) throws Throwable {
+			  return sessionData.put((String)invocation.getArguments()[0], invocation.getArguments()[1]);
+			}
+		});		
+		output = new TestEncoderOutput();
+  }
+	
+	@After
+	public void tearDown() {
+		output.close();
+	}
+  
+	@Test
+  public void testEncode() throws Exception{
+    String toEncode = "THIS_IS_A_TEST";
+    NioResponse res = new NioResponse();
+    res.setObject(toEncode);    
+
+    encoder.encode(session, res, output);
+    int length = output.buf.getInt();
+    byte[] bytes = new byte[length];
+    output.buf.get(bytes);
+
+    ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+    ObjectInputStream ois = MarshalStreamFactory.createInputStream(bis);
+    assertEquals(toEncode, ois.readObject());
+    
+  }
+
+}
+
+
