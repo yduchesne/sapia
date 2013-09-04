b0833babaaa64dc58b9d35dd78bc3aacc2b8f685
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2013-09-04 01:14:47 +0000

    [maven-release-plugin]  copy for tag sapia_corus_parent-4.1-RC4

---
+++ tags/sapia_corus_parent-4.1-RC4/modules/client/src/main/java/org/sapia/corus/client/services/db/persistence/Persistent.java
@@ -0,0 +1,28 @@
+package org.sapia.corus.client.services.db.persistence;
+
+import org.sapia.corus.client.annotations.Transient;
+import org.sapia.corus.client.services.db.DbMap;
+
+/**
+ * Implemented by persistent objects.
+ * 
+ * @author yduchesne
+ *
+ * @param <K> the generic type of the persistent object key.
+ * @param <V> the generic type of th persistent object value.
+ */
+public interface Persistent<K, V> {
+
+  @Transient
+  public K getKey();
+  
+  public void setDbMap(DbMap<K, V> db);
+  
+  public boolean save();
+  
+  public boolean delete();
+  
+  public void markDeleted();
+  
+  public boolean refresh();
+}
