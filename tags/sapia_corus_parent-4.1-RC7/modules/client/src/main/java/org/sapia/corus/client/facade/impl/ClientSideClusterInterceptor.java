24d5c32b38d0a3639a1b250722686fcc412a6e68
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2013-10-20 23:16:00 +0000

    [maven-release-plugin]  copy for tag sapia_corus_parent-4.1-RC7

---
+++ tags/sapia_corus_parent-4.1-RC7/modules/client/src/main/java/org/sapia/corus/client/facade/impl/ClientSideClusterInterceptor.java
@@ -0,0 +1,42 @@
+package org.sapia.corus.client.facade.impl;
+
+import org.sapia.corus.client.ClusterInfo;
+import org.sapia.corus.client.services.cluster.ClusteredCommand;
+import org.sapia.ubik.rmi.interceptor.Interceptor;
+import org.sapia.ubik.rmi.server.invocation.ClientPreInvokeEvent;
+import org.sapia.ubik.util.Assertions;
+
+
+/**
+ * An instance of this class is used on the client side by facade implementation
+ * in order to seamlessly cluster remote method calls.
+ * 
+ * @author Yanick Duchesne
+ */
+public class ClientSideClusterInterceptor implements Interceptor {
+  private static ThreadLocal<ClusterInfo> registration = new ThreadLocal<ClusterInfo>();
+
+  public static void clusterCurrentThread(ClusterInfo cluster) {
+    registration.set(cluster);
+  }
+
+  public void onClientPreInvokeEvent(ClientPreInvokeEvent evt) {
+    if (isCurrentThreadClustered()) {
+      ClusteredCommand command = new ClusteredCommand(evt.getCommand());
+      ClusterInfo cluster = registration.get();
+      Assertions.illegalState(cluster == null, "ClusterInfo instance not set");
+      command.addTargets(cluster.getTargets());
+      evt.setCommand(command);
+    }
+  }
+
+  private static boolean isCurrentThreadClustered() {
+		ClusterInfo clustered = registration.get();
+
+    if (clustered == null) {
+      return false;
+    } else {
+      return clustered.isClustered();
+    }
+  }
+}
