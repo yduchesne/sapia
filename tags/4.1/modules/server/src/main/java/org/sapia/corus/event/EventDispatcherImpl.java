2e78720b66d9bcd01d9e0ddc999120a7bfcde7bf
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2013-10-27 21:33:30 +0000

    [maven-release-plugin]  copy for tag 4.1

---
+++ tags/4.1/modules/server/src/main/java/org/sapia/corus/event/EventDispatcherImpl.java
@@ -0,0 +1,89 @@
+package org.sapia.corus.event;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import org.sapia.corus.client.annotations.Bind;
+import org.sapia.corus.client.services.event.EventDispatcher;
+import org.sapia.corus.core.ModuleHelper;
+import org.sapia.ubik.concurrent.ConfigurableExecutor;
+import org.sapia.ubik.concurrent.NamedThreadFactory;
+import org.sapia.ubik.concurrent.ConfigurableExecutor.ThreadingConfiguration;
+import org.sapia.ubik.rmi.interceptor.Event;
+import org.sapia.ubik.rmi.interceptor.Interceptor;
+import org.sapia.ubik.rmi.interceptor.MultiDispatcher;
+import org.sapia.ubik.util.Time;
+
+/**
+ * Implements the {@link EventDispatcher} interface.
+ * 
+ * @author Yanick Duchesne
+ */
+@Bind(moduleInterface=EventDispatcher.class)
+public class EventDispatcherImpl extends ModuleHelper implements EventDispatcher {
+  
+  private static final int CORE_POOL_SIZE      = 2;
+  private static final int MAX_POOL_SIZE       = 5;
+  private static final long KEEP_ALIVE_SECONDS = 30;
+  private static final int WORK_QUEUE_SIZE     = 1000;
+  
+  private MultiDispatcher delegate = new MultiDispatcher();
+  private ExecutorService executor;
+
+  @Override
+  public String getRoleName() {
+    return ROLE;
+  }
+
+  @Override
+  public void init() throws Exception {
+    ThreadingConfiguration conf = ThreadingConfiguration.newInstance()
+        .setCorePoolSize(CORE_POOL_SIZE)
+        .setMaxPoolSize(MAX_POOL_SIZE)
+        .setKeepAlive(Time.createSeconds(KEEP_ALIVE_SECONDS))
+        .setQueueSize(WORK_QUEUE_SIZE);
+    
+    executor = new ConfigurableExecutor(
+        conf, 
+        NamedThreadFactory
+          .createWith("EventDispatcher")
+          .setDaemon(true));
+  }
+
+  @Override
+  public void start() throws Exception {}
+
+  @Override
+  public void dispose() {
+    if (executor != null) {
+      executor.shutdownNow();
+    }
+  }
+  
+  @Override
+  @SuppressWarnings("rawtypes")
+  public void addInterceptor(Class event, Interceptor it){
+    logger().debug("Adding interceptor: " + it + " for event type: " + event);
+    delegate.addInterceptor(event, it);
+  }
+  
+  @Override
+  public void dispatch(final Event event) {
+    // precaution if this instance is used in unit testing and init()
+    // has not been called
+    if (executor == null) {
+      delegate.dispatch(event);
+    } else {
+      executor.execute(new Runnable() {
+        @Override
+        public void run() {
+          delegate.dispatch(event);
+        }
+      });
+    }
+  }
+
+}
