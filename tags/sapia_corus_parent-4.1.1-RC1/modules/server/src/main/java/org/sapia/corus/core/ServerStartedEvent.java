5ecb4071daee2d5429356da5970468a53aa641f3
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2013-11-11 01:37:50 +0000

    [maven-release-plugin]  copy for tag sapia_corus_parent-4.1.1-RC1

---
+++ tags/sapia_corus_parent-4.1.1-RC1/modules/server/src/main/java/org/sapia/corus/core/ServerStartedEvent.java
@@ -0,0 +1,28 @@
+package org.sapia.corus.core;
+
+import org.sapia.ubik.net.ServerAddress;
+import org.sapia.ubik.rmi.interceptor.Event;
+
+/**
+ * Dispatched by the corus server once it has started listening to request.
+ * 
+ * @author Yanick Duchesne
+ */
+public class ServerStartedEvent implements java.io.Serializable, Event{
+	
+  static final long serialVersionUID = 1L;
+  
+	private ServerAddress _address;
+
+	public ServerStartedEvent(ServerAddress address){
+		_address  = address; 
+	}
+	
+	/**
+	 * @return the <code>ServerAddress</code> that corresponds to the address of the 
+	 * corus server.
+	 */
+	public ServerAddress getAddress(){
+		return _address;
+	}
+}
