d07000a6a7903ca53f94d49926c6511576923a11
jcdesrochers@gmail.com <jcdesrochers@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2013-11-25 02:56:17 +0000

    Copied trunk/corus_iop to branches/corus_iop/fs0005-dump.

--- ubik/core/src/main/java/org/sapia/ubik/rmi/server/transport/RmiObjectOutput.java
+++
@@ -1,9 +0,0 @@
-package org.sapia.ubik.rmi.server.transport;
-
-import org.sapia.ubik.rmi.server.VmId;
-
-public interface RmiObjectOutput {
-
-  public void setUp(VmId id, String transportType);
-
-}
