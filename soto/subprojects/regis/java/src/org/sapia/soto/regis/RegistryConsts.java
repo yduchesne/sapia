4e371187eea31cd4114f8838b57cec96810d3a96
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2013-12-28 00:57:42 +0000

    [maven-release-plugin]  copy for tag sapia_resource-1.4

--- soto/subprojects/regis/java/src/org/sapia/soto/regis/RegistryConsts.java
+++
@@ -1,9 +0,0 @@
-package org.sapia.soto.regis;
-
-public interface RegistryConsts {
-
-  public static final String NODE_REF  = "@regis.node";
-  public static final String NODE_NAME = "@regis.node.name";  
-  public static final String NODE_PATH = "@regis.node.path";  
-  
-}
