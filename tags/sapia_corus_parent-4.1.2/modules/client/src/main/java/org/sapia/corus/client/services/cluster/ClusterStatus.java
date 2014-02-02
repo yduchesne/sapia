2e651a1c16877976412baff128e1003c7176e753
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2014-02-02 00:45:50 +0000

    [maven-release-plugin]  copy for tag sapia_corus_parent-4.1.2

---
+++ tags/sapia_corus_parent-4.1.2/modules/client/src/main/java/org/sapia/corus/client/services/cluster/ClusterStatus.java
@@ -0,0 +1,61 @@
+package org.sapia.corus.client.services.cluster;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+import org.sapia.ubik.mcast.EventChannel;
+import org.sapia.ubik.mcast.EventChannel.Role;
+
+/**
+ * Encapsulates the status in the cluster for a given Corus host.
+ * 
+ * @author yduchesne
+ * 
+ */
+public class ClusterStatus implements Externalizable {
+
+  static final long serialVersionUID = 1L;
+
+  private Role role;
+  private CorusHost host;
+
+  /** DO NOT USE: meant for externalization only. */
+  public ClusterStatus() {
+  }
+
+  public ClusterStatus(Role role, CorusHost host) {
+    this.role = role;
+    this.host = host;
+  }
+
+  /**
+   * @return the Corus instance's role.
+   */
+  public Role getRole() {
+    return role;
+  }
+
+  /**
+   * @return the Corus host information.
+   */
+  public CorusHost getHost() {
+    return host;
+  }
+
+  // --------------------------------------------------------------------------
+  // Externalizable interface
+
+  @Override
+  public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+    role = (EventChannel.Role) in.readObject();
+    host = (CorusHost) in.readObject();
+  }
+
+  @Override
+  public void writeExternal(ObjectOutput out) throws IOException {
+    out.writeObject(role);
+    out.writeObject(host);
+  }
+}
---
+++ tags/sapia_corus_parent-4.1.2/modules/server/src/main/java/org/sapia/corus/naming/example/TimeService.java
@@ -0,0 +1,12 @@
+package org.sapia.corus.naming.example;
+
+import java.util.Date;
+
+/**
+ * @author Yanick Duchesne
+ */
+public interface TimeService {
+
+  public Date getTime();
+
+}
