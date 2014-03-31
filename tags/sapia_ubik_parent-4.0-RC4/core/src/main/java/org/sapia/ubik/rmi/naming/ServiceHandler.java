5d334a8c0362a9ac2991505f1496f8a2d7cb30f6
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2014-03-31 02:34:09 +0000

    [maven-release-plugin]  copy for tag sapia_ubik_parent-4.0-RC4

---
+++ tags/sapia_ubik_parent-4.0-RC4/core/src/main/java/org/sapia/ubik/rmi/naming/ServiceHandler.java
@@ -0,0 +1,41 @@
+package org.sapia.ubik.rmi.naming;
+
+import java.util.Map;
+
+import javax.naming.NameNotFoundException;
+import javax.naming.NamingException;
+
+/**
+ * An instance of this class performs lookups on behalf of the
+ * {@link ServiceLocator}.
+ * 
+ * @see ServiceLocator
+ * 
+ * @author Yanick Duchesne
+ */
+public interface ServiceHandler {
+  /**
+   * Handles the lookup for the given path, given the host and port.
+   * 
+   * @param host
+   *          the host to look up.
+   * @param port
+   *          the port on which to connect. If the port corresponds to the
+   *          {@link ServiceLocator#UNDEFINED_PORT} constant, it means that the
+   *          caller has not specified any port (a necessary feature in order to
+   *          support well-known ports).
+   * @param path
+   *          the path of the object to look up.
+   * @param attributes
+   *          a <code>Map</code> of attributes - the latter correspond to the
+   *          query string parameters that is parsed out of the URL that is
+   *          specified to the {@link ServiceLocator}.
+   * 
+   * @return an <code>Object</code>
+   * @throws NameNotFoundException
+   *           if no object could be found for the given path.
+   * @throws NamingException
+   *           if an error occurs while performing the lookup.
+   */
+  public Object handleLookup(String host, int port, String path, Map<String, String> attributes) throws NameNotFoundException, NamingException;
+}
---
+++ tags/sapia_ubik_parent-4.0-RC4/core/src/test/java/org/sapia/ubik/net/mplex/HttpStreamSelectorTest.java
@@ -0,0 +1,107 @@
+package org.sapia.ubik.net.mplex;
+
+import java.io.UnsupportedEncodingException;
+
+import junit.framework.TestCase;
+
+/**
+ * @author <a href="mailto:jc@sapia-oss.org">Jean-Cedric Desrochers</a>
+ */
+public class HttpStreamSelectorTest extends TestCase {
+  private static final byte[] ubikHeader;
+  private static final byte[] simpleHttpHeader;
+  private static final byte[] complexHttpHeader;
+
+  static {
+    byte[] data1 = new byte[0];
+    byte[] data2 = new byte[0];
+    byte[] data3 = new byte[0];
+
+    try {
+      data1 = "UBIK!#@/$abcdefg".getBytes("UTF-8");
+      data2 = "POST / HTTP/1.1\nHost: localhost\n".getBytes("UTF-8");
+      data3 = "GET /server/mplex/test?param1=value1&param2=value2 HTTP/1.1\nHost: localhost\n".getBytes("UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      e.printStackTrace();
+    } finally {
+      ubikHeader = data1;
+      simpleHttpHeader = data2;
+      complexHttpHeader = data3;
+    }
+  }
+
+  /**
+   * Creates a new HttpStreamSelectorTest instance.
+   */
+  public HttpStreamSelectorTest(String aName) {
+    super(aName);
+  }
+
+  public void testDefaultSelector() throws Exception {
+    HttpStreamSelector selector = new HttpStreamSelector();
+    assertTrue(!selector.selectStream(ubikHeader));
+    assertTrue(selector.selectStream(simpleHttpHeader));
+    assertTrue(selector.selectStream(complexHttpHeader));
+  }
+
+  public void testHttpMethodSelector() throws Exception {
+    HttpStreamSelector selector = new HttpStreamSelector("POST", null);
+    assertTrue(!selector.selectStream(ubikHeader));
+    assertTrue(selector.selectStream(simpleHttpHeader));
+    assertTrue(!selector.selectStream(complexHttpHeader));
+
+    selector = new HttpStreamSelector("GET", null);
+    assertTrue(!selector.selectStream(ubikHeader));
+    assertTrue(!selector.selectStream(simpleHttpHeader));
+    assertTrue(selector.selectStream(complexHttpHeader));
+
+    selector = new HttpStreamSelector("OPTIONS", null);
+    assertTrue(!selector.selectStream(ubikHeader));
+    assertTrue(!selector.selectStream(simpleHttpHeader));
+    assertTrue(!selector.selectStream(complexHttpHeader));
+  }
+
+  public void testRequestPatternSelector() throws Exception {
+    HttpStreamSelector selector = new HttpStreamSelector(null, "/");
+    assertTrue(!selector.selectStream(ubikHeader));
+    assertTrue(selector.selectStream(simpleHttpHeader));
+    assertTrue(selector.selectStream(complexHttpHeader));
+
+    selector = new HttpStreamSelector(null, "/server/mplex");
+    assertTrue(!selector.selectStream(ubikHeader));
+    assertTrue(!selector.selectStream(simpleHttpHeader));
+    assertTrue(selector.selectStream(complexHttpHeader));
+
+    selector = new HttpStreamSelector(null, "/ubik");
+    assertTrue(!selector.selectStream(ubikHeader));
+    assertTrue(!selector.selectStream(simpleHttpHeader));
+    assertTrue(!selector.selectStream(complexHttpHeader));
+  }
+
+  public void testHttpMethodAndRequestPatternSelector() throws Exception {
+    HttpStreamSelector selector = new HttpStreamSelector("POST", "/");
+    assertTrue(!selector.selectStream(ubikHeader));
+    assertTrue(selector.selectStream(simpleHttpHeader));
+    assertTrue(!selector.selectStream(complexHttpHeader));
+
+    selector = new HttpStreamSelector("GET", "/");
+    assertTrue(!selector.selectStream(ubikHeader));
+    assertTrue(!selector.selectStream(simpleHttpHeader));
+    assertTrue(selector.selectStream(complexHttpHeader));
+
+    selector = new HttpStreamSelector("POST", "/server/mplex");
+    assertTrue(!selector.selectStream(ubikHeader));
+    assertTrue(!selector.selectStream(simpleHttpHeader));
+    assertTrue(!selector.selectStream(complexHttpHeader));
+
+    selector = new HttpStreamSelector("GET", "/server/mplex");
+    assertTrue(!selector.selectStream(ubikHeader));
+    assertTrue(!selector.selectStream(simpleHttpHeader));
+    assertTrue(selector.selectStream(complexHttpHeader));
+
+    selector = new HttpStreamSelector("POST", "/ubik");
+    assertTrue(!selector.selectStream(ubikHeader));
+    assertTrue(!selector.selectStream(simpleHttpHeader));
+    assertTrue(!selector.selectStream(complexHttpHeader));
+  }
+}
