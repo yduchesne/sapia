cacd7abf3f2c02514c8c1bce74d267cae5480c90
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2014-03-31 02:30:59 +0000

    Removed tags/sapia_ubik_parent-4.0-RC4

--- tags/sapia_ubik_parent-4.0-RC4/core/src/main/java/org/sapia/ubik/rmi/server/Stateless.java
+++
@@ -1,9 +0,0 @@
-package org.sapia.ubik.rmi.server;
-
-/**
- * A marker interface that identifies stateless stubs.
- * 
- * @author Yanick Duchesne
- */
-public interface Stateless {
-}
--- tags/sapia_ubik_parent-4.0-RC4/core/src/main/java/org/sapia/ubik/rmi/server/transport/socket/MultiplexSocketConnectionFactory.java
+++
@@ -1,9 +0,0 @@
-package org.sapia.ubik.rmi.server.transport.socket;
-
-public class MultiplexSocketConnectionFactory extends SocketRmiConnectionFactory {
-
-  public MultiplexSocketConnectionFactory() {
-    super(MultiplexSocketTransportProvider.MPLEX_TRANSPORT_TYPE);
-  }
-
-}
