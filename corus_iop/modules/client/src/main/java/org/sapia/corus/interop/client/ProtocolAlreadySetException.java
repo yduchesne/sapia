61b42fba7ece2e085dc4c66de45eb9767de36823
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2014-04-18 21:08:45 +0000

    [maven-release-plugin]  copy for tag sapia_ubik_parent-4.0-RC6

--- corus_iop/modules/client/src/main/java/org/sapia/corus/interop/client/ProtocolAlreadySetException.java
+++
@@ -1,9 +0,0 @@
-package org.sapia.corus.interop.client;
-
-public class ProtocolAlreadySetException extends IllegalStateException{
-  
-  public ProtocolAlreadySetException() {
-    super("Interop protocol implementation already set");
-  }
-
-}
