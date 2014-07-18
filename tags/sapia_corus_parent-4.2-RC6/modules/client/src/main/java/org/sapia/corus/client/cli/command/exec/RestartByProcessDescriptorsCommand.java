9c100444ca6fa6a3e022e7e9fba75db402650887
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2014-07-18 04:38:49 +0000

    [maven-release-plugin]  copy for tag sapia_corus_parent-4.2-RC6

---
+++ tags/sapia_corus_parent-4.2-RC6/modules/client/src/main/java/org/sapia/corus/client/cli/command/exec/RestartByProcessDescriptorsCommand.java
@@ -0,0 +1,46 @@
+package org.sapia.corus.client.cli.command.exec;
+
+import org.sapia.console.AbortException;
+import org.sapia.console.CmdLine;
+import org.sapia.console.InputException;
+import org.sapia.console.Option;
+import org.sapia.corus.client.ClusterInfo;
+import org.sapia.corus.client.cli.CliContext;
+import org.sapia.corus.client.cli.command.Restart;
+import org.sapia.corus.client.services.processor.KillPreferences;
+import org.sapia.corus.client.services.processor.ProcessCriteria;
+
+/**
+ * Implements the logic for restarting processes by descriptors (
+ * <code>restart -d * -v * -n * -p test</code>).
+ * 
+ * @author yduchesne
+ * 
+ */
+public class RestartByProcessDescriptorsCommand extends RestartAndWaitCommandSupport {
+
+  @Override
+  protected void doExecute(CliContext ctx) throws AbortException, InputException {
+
+    CmdLine cmd = ctx.getCommandLine();
+    String distName = cmd.assertOption(DIST_OPT, true).getValue();
+    String version = cmd.assertOption(VERSION_OPT, true).getValue();
+    String processName = cmd.containsOption(VM_NAME_OPT, false) ? cmd.assertOption(VM_NAME_OPT, true).getValue() : null;
+    String profile = cmd.containsOption(PROFILE_OPT, false) ? cmd.assertOption(PROFILE_OPT, true).getValue() : null;
+
+    ClusterInfo cluster = getClusterInfo(ctx);
+
+    ProcessCriteria criteria = ProcessCriteria.builder().name(processName).distribution(distName).version(version).profile(profile).build();
+
+    Option wait = getWaitOption(ctx);
+    KillPreferences prefs = KillPreferences.newInstance().setHard(isHardKillOption(ctx));
+    if (wait != null) {
+      ctx.getConsole().println("Waiting for process restart, please stand by...");
+      doRestartAndWait(ctx, cluster, criteria, prefs, wait.getValue() == null ? Restart.DEFAULT_RESTART_WAIT_TIME_SECONDS : wait.asInt());
+    } else {
+      ctx.getConsole().println("Triggering process restart...");
+      ctx.getCorus().getProcessorFacade().restart(criteria, prefs, getClusterInfo(ctx));
+    }
+  }
+
+}
