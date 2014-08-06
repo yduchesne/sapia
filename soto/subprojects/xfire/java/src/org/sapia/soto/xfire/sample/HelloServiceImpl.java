2ce36f0fa5c686ea9c26efbab33940a997877b3b
yanickduchesne@gmail.com <yanickduchesne@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2014-08-06 03:52:58 +0000

    [maven-release-plugin]  copy for tag sapia_corus_parent-4.2.1

--- soto/subprojects/xfire/java/src/org/sapia/soto/xfire/sample/HelloServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package org.sapia.soto.xfire.sample;
-
-public class HelloServiceImpl implements HelloService{
-  
-  public String getGreeting(String firstName, String lastName) {
-    return GREETING;
-  }
-
-}
