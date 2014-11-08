786e4cadbc08fb5624a556bd7d6897d5f74e0bd4
jcdesrochers@gmail.com <jcdesrochers@gmail.com@5eff64f3-1e4c-0410-aaf5-d36480b9dcb2>
2014-11-08 16:26:12 +0000

    Merged from trunk and revised pom files

--- fs0005-dump/modules/client/src/main/java/org/sapia/corus/interop/client/StdoutLog.java
+++ fs0005-dump/modules/client/src/main/java/org/sapia/corus/interop/client/StdoutLog.java
@@ -2,16 +2,9 @@ package org.sapia.corus.interop.client;
 
 
 /**
- * Implements the <code>Log</code> interface over stdout. The default
- * log level is INFO.
+ * Implements the {@link Log} interface over <code>stdout</code>. The default log level is INFO.
  *
  * @author Yanick Duchesne
- *
- * <dl>
- * <dt><b>Copyright:</b><dd>Copyright &#169; 2002-2003 <a href="http://www.sapia-oss.org">Sapia Open Source Software</a>. All Rights Reserved.</dd></dt>
- * <dt><b>License:</b><dd>Read the license.txt file of the jar or visit the
- *        <a href="http://www.sapia-oss.org/license.html">license page</a> at the Sapia OSS web site</dd></dt>
- * </dl>
  */
 public class StdoutLog implements Log {
   public static final int DEBUG   = 0;
